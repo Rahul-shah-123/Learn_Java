216779f10caee092940e486fd129bbbbf5d0920b
Rahul-shah-123 <69796495+Rahul-shah-123@users.noreply.github.com>
2025-01-13 00:09:55 +0530

    new commit

--- strings/stringBasic.java
+++ strings/stringBasic.java
@@ -2,6 +2,7 @@ package strings;
 
 public class stringBasic {
     public static void main(String[] args){
-
+        String name = "Rahul Shah";
+        System.out.println("Length of String = " + name.length());
     }
 }
