f31a8bea4e27d0a3fd52f465358ad44f1ca0db84
Rahul-shah-123 <69796495+Rahul-shah-123@users.noreply.github.com>
2025-01-14 23:00:13 +0530

    String topic complete

---
+++ strings/addTheLength.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class addTheLength {
+    public static void main(String[] args){
+        String name = "Rahul";
+        String surname = "Shah";
+        System.out.println(name.length() + surname.length());
+    }
+}
---
+++ strings/subString.java
@@ -0,0 +1,10 @@
+package strings;
+
+public class subString {
+    public static void main(String[] args){
+        String s = "hello, Rahul!";
+        int i = 8;
+        String str = s.substring(0, i);
+        System.out.println(str);
+    }
+}
