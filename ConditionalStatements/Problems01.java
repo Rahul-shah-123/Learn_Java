a2c02b6e32553750ccb823da32e156a8399ab43f
Rahul-shah-123 <rahulshahj27@gmail.com>
2025-02-09 15:30:46 +0530

    rahul

--- ConditionalStatements/Problems01.java
+++ ConditionalStatements/Problems01.java
@@ -2,12 +2,13 @@ package ConditionalStatements;
 
 import java.util.Scanner;
 
-public class Problems01 {
+public class Problems01 {Rahul-shah-123
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // Take all inputs
         String s = sc.nextLine();
         int a = sc.nextInt();
+
         float b = sc.nextFloat();
         long c = sc.nextLong();
         byte d = sc.nextByte();
